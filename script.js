04f1fbc9d44cf03f483ba316f1d10b81c9da02d8
Edgar Parucho <e.parucho@outlook.com>
2024-07-05 13:38:53 -0400

    Configura estilos dinámicos en caso de no existir texto para procesar

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const inputElement = document.getElementById('input-element');
 const outputElement = document.getElementById('output-element');
+const defaultOutputMessage = 'Ningún mensaje encontrado';
+const defaultOutputStyles = "background-image: url('./assets/Not_found.svg'); background-repeat: no-repeat; background-position: 100px 0px; background-blend-mode: lighten; background-size: cover;"
 
 const charsEncryption = {
   a: 'ai',
@@ -9,18 +11,28 @@ const charsEncryption = {
   u: 'ufat',
 };
 
+function setDefaultStatus() {
+  document.getElementById('output-card').setAttribute('style', defaultOutputStyles);
+  outputElement.innerText = defaultOutputMessage;
+}
+
 function encryptString(string) {
-  let encriptedString = '';
-  for (let char of string) encriptedString += charsEncryption[char] || char;
-  return encriptedString;
+  if (string == '') return setDefaultStatus();
+  let encryptedString = '';
+  for (let char of string) encryptedString += charsEncryption[char] || char;
+  outputElement.innerText = encryptedString;
+  document.getElementById('output-card').removeAttribute('style');
 }
 
 function decryptString(string) {
-  let decriptedString = string;
-  for (let char in charsEncryption) decriptedString = decriptedString.replaceAll(charsEncryption[char], char);
-  return decriptedString;
+  if (string == '') return setDefaultStatus();
+  let decryptedString = string;
+  for (let char in charsEncryption) decryptedString = decryptedString.replaceAll(charsEncryption[char], char);
+  outputElement.innerText = decryptedString;
+  document.getElementById('output-card').removeAttribute('style');
 }
 
-document.getElementById('encrypt-button').addEventListener('click', () => outputElement.innerText = encryptString(inputElement.value));
-document.getElementById('decrypt-button').addEventListener('click', () => outputElement.innerText = decryptString(inputElement.value));
+document.getElementById('encrypt-button').addEventListener('click', () => encryptString(inputElement.value));
+document.getElementById('decrypt-button').addEventListener('click', () => decryptString(inputElement.value));
 document.getElementById('copy-button').addEventListener('click', () => navigator.clipboard.writeText(outputElement.innerText));
+setDefaultStatus();
